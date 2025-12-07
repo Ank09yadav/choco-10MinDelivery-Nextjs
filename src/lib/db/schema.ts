d6e41504918945cdc69dd6d3b901934589a9e621
ANK <ankury114112118@gmail.com>
2025-12-07 11:45:34 +0530

    product schema done

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,5 +1,5 @@
 
-import {pgTable,varchar, serial, text, timestamp} from "drizzle-orm/pg-core";
+import {pgTable,varchar, serial, text, timestamp, integer} from "drizzle-orm/pg-core";
 import {sql} from "drizzle-orm";
 
 export const users = pgTable("users", {
@@ -11,6 +11,17 @@ export const users = pgTable("users", {
     externalId: varchar("external_id",{length:100}).notNull(),
     image:text("image"),
     role: varchar("role",{length:12}).notNull().default("customer"),
+    createdAt: timestamp("created_at").notNull().default(sql`CURRENT_TIMESTAMP`),
+    updatedAt: timestamp("updated_at").notNull().default(sql`CURRENT_TIMESTAMP`),
+});
+
+
+export const products = pgTable("products", {
+    id: serial("id").primaryKey(),
+    name:varchar("name",{length:100}).notNull(),
+    image:text("image").notNull(),
+    description:text("description").notNull(),
+    price: integer("price").notNull(),
     createdAt: timestamp("created_at").notNull().default(sql`CURRENT_TIMESTAMP`),
     updatedAt: timestamp("updated_at").notNull().default(sql`CURRENT_TIMESTAMP`),
 });
