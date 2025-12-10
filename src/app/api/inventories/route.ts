75dc0e1b06ba7d08f88bae39b920a46d6846237e
ANK <ankury114112118@gmail.com>
2025-12-10 11:21:56 +0530

    get all inventories endpoint

--- src/app/api/inventories/route.ts
+++ src/app/api/inventories/route.ts
@@ -1,6 +1,8 @@
 import { db } from "@/lib/db/db";
-import { inventories } from "@/lib/db/schema";
+import { inventories, products, warehouse } from "@/lib/db/schema";
 import { inventorySchema } from "@/lib/validators/inventorySchema";
+import { productSchema } from "@/lib/validators/productSchema";
+import { desc, eq } from "drizzle-orm";
 
 export async function POST(request: Request) {
     const requestData = await request.json();
@@ -20,5 +22,23 @@ export async function POST(request: Request) {
     } catch (error) {
         console.log(error);
         return Response.json({message:"Failed to store inventory."}, {status:500});
+    }
+}
+
+export async function GET() {
+    try {
+        const allInventories= await db.select({
+            id:inventories.id,
+            sku:inventories.sku,
+            warehouse:warehouse.name,
+            product:products.name
+        })
+        .from(inventories)
+        .leftJoin(warehouse,eq(inventories.warehouseId,warehouse.id))
+        .leftJoin(products,eq(inventories.productId,products.id))
+        .orderBy(desc(inventories.id));
+        return Response.json({inventories:allInventories}, {status:200});
+    }catch (error) {
+        return Response.json({message:"Failed to fetch inventories."}, {status:500});
     }
 }
