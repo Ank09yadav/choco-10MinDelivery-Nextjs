f37f02e4c432085afcb09133cce103079b93eda1
Ank09yadav <ankury114112118@gmail.com>
2026-03-16 23:35:45 +0530

    added warehouse page

---
+++ src/app/admin/warehouse/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Warehouse = ()=>{
+    
+    return(
+        <div>
+            <p>this is warehouse page </p>
+        </div>
+    )
+}
+
+export default Warehouse;
