41100ebfb93a9dd28007649d0c80e847d7092e11
Ank09yadav <ankury114112118@gmail.com>
2026-03-26 01:39:47 +0530

    added delivery persons page

---
+++ src/app/admin/deliver-persons/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DeliveryPersonsPage = () => {
+  return (
+    <div>DeliveryPersonsPage</div>
+  )
+}
+
+export default DeliveryPersonsPage
--- src/app/api/inventories/route.ts
+++ src/app/api/inventories/route.ts
@@ -18,7 +18,7 @@ export async function POST(request: Request) {
 
     try {
         await db.insert(inventories).values(validatedData);
-        return Response.json({message: "Inventory created successfully"}, {status: 201});   
+        return Response.json({message: "Inventory created successfully"}, {status: 200});   
     } catch (error) {
         return Response.json({message: "Failed to create inventory"}, {status: 500});
     }
@@ -39,7 +39,7 @@ export async function GET(){
         }
 
         ).from(inventories).leftJoin(warehouses, eq(inventories.warehouseId, warehouses.id)).leftJoin(products, eq(inventories.productId, products.id)).orderBy(desc(inventories.id));
-        return Response.json({inventories: allInventories}, {status: 201});
+        return Response.json({inventories: allInventories}, {status: 200});
     }catch(error){
         return Response.json({message: "Failed to fetch inventories"}, {status: 500});
     }
