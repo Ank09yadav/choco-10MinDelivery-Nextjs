d9b6e62aefa46478c16cf34c9eb1dfb05f27ebca
Ank09yadav <ankury114112118@gmail.com>
2026-04-01 23:44:29 +0530

    added hero section

---
+++ src/app/(client)/_components/hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Image from 'next/image';
+const Hero = () => {
+  return (
+    <div className='flex bg-[url("/choco.jpg")] bg-cover bg-center h-screen w-full items-center justify-center'>
+      
+    </div>
+  )
+}
+
+export default Hero;           
--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 
 import Header from "./_components/header";
+import Hero from "./_components/hero";
 
 
 export default function Home() {
   return (
     <>
      <Header/>
+     <Hero/>
 
     </>
   );
